72d848fe017bc89c5a908efd27caa05b7eea130d
Dinner <junghwan3588@gm.gist.ac.kr>
2022-10-17 18:11:24 +0900

    Design:iphone:: #5 Add WorkingCategorySelectionView

---
+++ Samsam/ViewController/WorkingSelectionViewController.swift
@@ -0,0 +1,72 @@
+//
+//  WorkingSelectionView.swift
+//  Samsam
+//
+//  Created by creohwan on 2022/10/17.
+//
+
+import UIKit
+
+class WorkingSelectionViewController: UIViewController {
+    
+    // MARK: - View
+    let categoryView: UICollectionView = {
+        $0.backgroundColor = .black
+        return $0
+    }(UICollectionView(
+        frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        attribute()
+
+        categoryView.delegate = self
+        categoryView.dataSource = self
+        
+        categoryView.register(CategoryCell.self, forCellWithReuseIdentifier: CategoryCell.identifier)
+    }
+    
+    // MARK: - Method
+
+    private func attribute() {
+        self.view.backgroundColor = .blue
+        view.addSubview(categoryView)
+        categoryView.anchor(
+            top: view.topAnchor,
+            left: view.leftAnchor,
+            bottom: view.bottomAnchor,
+            right: view.rightAnchor
+        )
+    }
+}
+
+// MARK: - UICollectionViewDelegate, DataSourse, DelegateFlowLayout
+extension WorkingSelectionViewController:  UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CategoryCell.identifier, for: indexPath) as! CategoryCell
+        
+        cell.backgroundColor = .purple
+        cell.categoryImage.image = UIImage(named:"category")
+        cell.categoryTitle.text = "카테고리 텍스트"
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth =  (view.frame.width - 48)/3
+        let cellHeight = 120
+        return CGSize(width: Int(cellWidth), height: Int(cellHeight))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+         return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    }
+    
+
+}
