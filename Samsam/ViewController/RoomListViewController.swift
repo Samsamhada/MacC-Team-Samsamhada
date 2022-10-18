f2bc2a4c47e2965e28fd65e5c8c3094715523d88
taek0622 <min4644@gmail.com>
2022-10-18 16:03:55 +0900

    Design:iphone:: #7 Modify RoomListView Layout

--- Samsam/ViewController/RoomListViewController.swift
+++ Samsam/ViewController/RoomListViewController.swift
@@ -11,6 +11,21 @@ class RoomListViewController: UIViewController {
     
     // MARK: - View
     
+    private let creationButton: UIButton = {
+        $0.setImage(UIImage(systemName: "plus"), for: .normal)
+        $0.tintColor = .black
+        $0.setTitle("고객 추가하기", for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        $0.setTitleColor(.black, for: .normal)
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 16
+        $0.layer.shadowColor = UIColor.black.cgColor
+        $0.layer.shadowOpacity = 0.2
+        $0.layer.shadowOffset = CGSize(width: 4, height: 4)
+        $0.layer.shadowRadius = 10
+        return $0
+    }(UIButton())
+    
     private let collectionView: UICollectionView = {
         return $0
     }(UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()))
@@ -34,10 +49,22 @@ class RoomListViewController: UIViewController {
     }
     
     private func layout() {
+        view.addSubview(creationButton)
         view.addSubview(collectionView)
         
-        collectionView.anchor(
+        creationButton.anchor(
             top: view.safeAreaLayoutGuide.topAnchor,
+            left: view.safeAreaLayoutGuide.leftAnchor,
+            bottom: collectionView.topAnchor,
+            right: view.safeAreaLayoutGuide.rightAnchor,
+            paddingTop: 16,
+            paddingLeft: 16,
+            paddingBottom: 16,
+            paddingRight: 16,
+            height: 90
+        )
+        
+        collectionView.anchor(
             left: view.safeAreaLayoutGuide.leftAnchor,
             bottom: view.safeAreaLayoutGuide.bottomAnchor,
             right: view.safeAreaLayoutGuide.rightAnchor
@@ -69,6 +96,6 @@ extension RoomListViewController: UICollectionViewDataSource, UICollectionViewDe
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: UIScreen.main.bounds.width - 32, height: 60)
+        return CGSize(width: UIScreen.main.bounds.width - 32, height: 90)
     }
 }
