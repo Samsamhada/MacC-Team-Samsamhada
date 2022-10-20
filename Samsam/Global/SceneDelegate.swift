f89530526a10d24281b06662b0ba31ce301f7016
Dinner <junghwan3588@gm.gist.ac.kr>
2022-10-20 10:40:12 +0900

    Chore:wrench:: #5 Modify ScenDelegate

--- Samsam/Global/SceneDelegate.swift
+++ Samsam/Global/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let navi = UINavigationController(rootViewController: PostingCategoryViewController())
+        let navi = UINavigationController(rootViewController: ViewController())
         window?.rootViewController = navi
         window?.makeKeyAndVisible()
     }
