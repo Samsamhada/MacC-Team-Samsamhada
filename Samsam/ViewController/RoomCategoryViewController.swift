0babb52647eaae100e5457821655d52510c294cc
Dinner <junghwan3588@gm.gist.ac.kr>
2022-10-21 11:54:21 +0900

    Design:iphone:: #36 Add TextTitle

--- Samsam/ViewController/RoomCategoryViewController.swift
+++ Samsam/ViewController/RoomCategoryViewController.swift
@@ -8,19 +8,42 @@
 import UIKit
 
 class RoomCategoryViewController: UIViewController {
+    
+    // MARK: - View
 
+    private var textTitle: UILabel = {
+        $0.text = "시공 항목을 모두 선택해주세요"
+        $0.textAlignment = .center
+        $0.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        $0.textColor = .black
+        return $0
+    }(UILabel())
 
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        layout()
         attribute()
-
-
     }
     
     // MARK: - Method
     
     private func attribute() {
+        view.backgroundColor = .white
         setNavigationTitle()
+    }
+    
+    private func layout() {
+        view.addSubview(textTitle)
+        
+        textTitle.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            left: view.safeAreaLayoutGuide.leftAnchor,
+            right: view.safeAreaLayoutGuide.rightAnchor,
+            paddingTop: 30,
+            height: 20
+        )
     }
     
     private func setNavigationTitle() {
