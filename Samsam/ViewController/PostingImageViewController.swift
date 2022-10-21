7011c22e8f97813cce0fccc70239ff2dcf0bbaa4
Dinner <junghwan3588@gm.gist.ac.kr>
2022-10-21 17:49:11 +0900

    Design:iphone:: #34 Add NavigationTitle

--- Samsam/ViewController/PostingImageViewController.swift
+++ Samsam/ViewController/PostingImageViewController.swift
@@ -9,8 +9,29 @@ import UIKit
 
 class PostingImageViewController: UIViewController {
     
+    // MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        attribute()
+        
+    }
+    
+    // MARK: - Method
+    
+    private func attribute() {
+        self.view.backgroundColor = .white
+        setNavigationTitle()
+    }
+    
+    private func setNavigationTitle() {
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .white
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.topItem?.title = "시공 상황 작성"
+        navigationController?.navigationBar.prefersLargeTitles = false
+        navigationController?.setNavigationBarHidden(false, animated: false)
     }
 }
