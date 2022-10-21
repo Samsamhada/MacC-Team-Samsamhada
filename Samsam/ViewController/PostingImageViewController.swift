34fcf234721522339bf1aa01da342ccc3f48b9bc
Dinner <junghwan3588@gm.gist.ac.kr>
2022-10-21 17:51:49 +0900

    Design:iphone:: #34 Add nextBTN

--- Samsam/ViewController/PostingImageViewController.swift
+++ Samsam/ViewController/PostingImageViewController.swift
@@ -9,12 +9,24 @@ import UIKit
 
 class PostingImageViewController: UIViewController {
     
+    // MARK: - View
+    
+    private let nextBTN: UIButton = {
+        $0.backgroundColor = .blue
+        $0.setTitle("다음", for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        $0.setTitleColor(.black, for: .normal)
+        $0.layer.cornerRadius = 16
+        $0.addTarget(self, action: #selector(tapNextBTN(_sender:)), for: .touchUpInside)
+        return $0
+    }(UIButton())
+    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        layout()
         attribute()
-        
     }
     
     // MARK: - Method
@@ -22,6 +34,19 @@ class PostingImageViewController: UIViewController {
     private func attribute() {
         self.view.backgroundColor = .white
         setNavigationTitle()
+    }
+    
+    private func layout() {
+        self.view.addSubview(nextBTN)
+        
+        nextBTN.anchor(
+            left: view.safeAreaLayoutGuide.leftAnchor,
+            bottom: view.safeAreaLayoutGuide.bottomAnchor,
+            right: view.safeAreaLayoutGuide.rightAnchor,
+            paddingLeft: 16,
+            paddingRight: 16,
+            height: 50
+        )
     }
     
     private func setNavigationTitle() {
@@ -33,5 +58,10 @@ class PostingImageViewController: UIViewController {
         navigationController?.navigationBar.topItem?.title = "시공 상황 작성"
         navigationController?.navigationBar.prefersLargeTitles = false
         navigationController?.setNavigationBarHidden(false, animated: false)
+    }
+    
+    @objc func tapNextBTN(_sender: UIButton) {
+        let vc = ViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
