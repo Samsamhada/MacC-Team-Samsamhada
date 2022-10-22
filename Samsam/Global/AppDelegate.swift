5f7162dace09ed94f37405d7901d4324cb802baf
taek0622 <min4644@gmail.com>
2022-10-23 02:28:44 +0900

    Feat:sparkles:: #44 Add CoreData at AppDelegate
    - CoreData 사용을 위해 AppDelegate에 CoreData 부분 추가

--- Samsam/Global/AppDelegate.swift
+++ Samsam/Global/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by 김민택 on 2022/10/17.
 //
 
+import CoreData
 import UIKit
 
 @main
@@ -22,7 +23,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
     }
+    
+    // MARK: - Core Data stack
+        lazy var persistentContainer: NSPersistentContainer = {
+            let container = NSPersistentContainer(name: "DataTable")
+            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+                if let error = error as NSError? {
+                    fatalError("Unresolved error \(error), \(error.userInfo)")
+                }
+            })
+            return container
+        }()
 
-
+        // MARK: - Core Data Saving support
+        func saveContext () {
+            let context = persistentContainer.viewContext
+            if context.hasChanges {
+                do {
+                    try context.save()
+                } catch {
+                    let nserror = error as NSError
+                    fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                }
+            }
+        }
 }
 
