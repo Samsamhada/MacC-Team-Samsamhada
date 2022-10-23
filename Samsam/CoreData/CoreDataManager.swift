337735c447f5d39bae2ba72b3171b092fa77b27d
taek0622 <min4644@gmail.com>
2022-10-24 04:01:51 +0900

    Feat:sparkles:: #44 Add CoreDataManager
    - 코어 데이터를 쉽게 사용, 관리하기 위한 코어 데이터 매니저 추가

---
+++ Samsam/CoreData/CoreDataManager.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataManager.swift
+//  Samsam
+//
+//  Created by 김민택 on 2022/10/23.
+//
+
+import CoreData
+import Foundation
+import UIKit
+class CoreDataManager {
+    
+    // MARK: - Property
+    
+    // MARK: - Save Method
+    
+    
+    // MARK: - Update Method
+    
+    
+    // MARK: - Load Method
+    
+    
+    // MARK: - Count Method
+    
+}
