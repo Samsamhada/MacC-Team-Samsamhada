c72ff5618322765c8a5a8fb84891b2963a07d098
Dinner <junghwan3588@gm.gist.ac.kr>
2022-10-25 16:15:07 +0900

    Feat:sparkles:: #34 Add Photo Right Alarm

--- Samsam/ViewController/PostingImageViewController.swift
+++ Samsam/ViewController/PostingImageViewController.swift
@@ -58,6 +58,7 @@ class PostingImageViewController: UIViewController {
         super.viewDidLoad()
         layout()
         attribute()
+        PHPhotoLibrary.requestAuthorization { (state) in }
     }
     
     // MARK: - Method
