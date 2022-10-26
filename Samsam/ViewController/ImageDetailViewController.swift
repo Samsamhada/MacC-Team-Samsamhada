b83db93e1a1ed55cd2af35609024244393400d7a
Juny0110 <wnsdyds403@naver.com>
2022-10-26 14:19:00 +0900

    Design:iphone:: #60 Add ImageDetailView layout

---
+++ Samsam/ViewController/ImageDetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ImageDetailViewController.swift
+//  Samsam
+//
+//  Created by 지준용 on 2022/10/25.
+//
+
+import UIKit
+
+class ImageDetailViewController: UIViewController {
+    
+    private var isTapped = false
+    
+    // MARK: - View
+    
+    private let uiView: UIView = {
+        return $0
+    }(UIView())
+    
+    private let detailImage: UIImageView = {
+        $0.image = UIImage(named: "TestImage")
+        $0.contentMode = .scaleAspectFit
+        return $0
+    }(UIImageView())
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+        attribute()
+    }
+    
+    // MARK: - Method
+    
+    private func layout() {
+        view.addSubview(detailImage)
+
+        detailImage.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            left: view.safeAreaLayoutGuide.leftAnchor,
+            bottom: view.safeAreaLayoutGuide.bottomAnchor,
+            right: view.safeAreaLayoutGuide.rightAnchor
+        )
+    }
+    
+    private func attribute() {
+        view.backgroundColor = .white
+        navigationItem.title = "화장실"
+    }
+}
+
