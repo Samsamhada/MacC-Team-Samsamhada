659510e94af4d49dd78a689d662306396f2f5943
Juny0110 <wnsdyds403@naver.com>
2022-10-27 23:43:09 +0900

    Design:iphone:: #78 Change PostingWritingView ButtonColor

--- Samsam/ViewController/PostingWritingView.swift
+++ Samsam/ViewController/PostingWritingView.swift
@@ -54,10 +54,10 @@ class PostingWritingView: UIViewController {
     }(UIView())
     
     private let finalBTN: UIButton = {
-        $0.backgroundColor = .blue
+        $0.backgroundColor = appColor.campanulaBlue
         $0.setTitle("작성 완료", for: .normal)
         $0.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
-        $0.setTitleColor(.black, for: .normal)
+        $0.setTitleColor(.white, for: .normal)
         $0.layer.cornerRadius = 16
         $0.addTarget(self, action: #selector(tapNextBTN), for: .touchUpInside)
         return $0
