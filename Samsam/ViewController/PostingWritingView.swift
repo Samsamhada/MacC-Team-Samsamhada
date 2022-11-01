352748b4cebc014636c9b2c2825c41ff73b5d36a
Dinner <junghwan3588@gm.gist.ac.kr>
2022-11-02 00:00:35 +0900

    Feat:sparkles:: #87 Modify Phicker

--- Samsam/ViewController/PostingWritingView.swift
+++ Samsam/ViewController/PostingWritingView.swift
@@ -13,7 +13,7 @@ class PostingWritingView: UIViewController {
     
     var roomID: Int?
     var categoryID: Int?
-    var imgItems: [PreviewItem]?
+//    var imgItems: [PreviewItem]?
     private let textViewPlaceHolder = "텍스트를 입력하세요"
 
     // MARK: - View
@@ -135,9 +135,9 @@ class PostingWritingView: UIViewController {
     
     @objc func tapNextBTN() {
         coreDataManager.createPostingData(roomID: roomID!, categoryID: categoryID!, explanation: textContent.text!)
-        imgItems?.forEach {
-            coreDataManager.createPhotoData(postingID: coreDataManager.countData(dataType: "posting") - 1, photoPath: $0.path!)
-        }
+//        imgItems?.forEach {
+//            coreDataManager.createPhotoData(postingID: coreDataManager.countData(dataType: "posting") - 1, photoPath: $0.path!)
+//        }
         self.dismiss(animated: true)
     }
     
