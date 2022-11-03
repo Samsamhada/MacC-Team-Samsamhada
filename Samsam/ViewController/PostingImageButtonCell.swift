28e2b9f63e363853f19eea2d3b49e823a65767cd
Dinner <junghwan3588@gm.gist.ac.kr>
2022-11-03 15:14:22 +0900

    Feat:sparkles:: #87 Add ButtonImageCell

---
+++ Samsam/ViewController/PostingImageButtonCell.swift
@@ -0,0 +1,53 @@
+//
+//  PostingImageButtonCell.swift
+//  Samsam
+//
+//  Created by creohwan on 2022/11/03.
+//
+
+import UIKit
+
+class PostingImageButtonCell: UICollectionViewCell {
+    
+    // MARK: - Property
+    
+    static let identifier = "PostingImageButtonCell"
+
+    // MARK: - View
+
+    private var buttomImage: UIImageView = {
+        let imageConfig = UIImage.SymbolConfiguration(pointSize: 60, weight: .light)
+        let image = UIImage(systemName: "plus.circle", withConfiguration: imageConfig)?.withTintColor(.white, renderingMode: .alwaysOriginal)
+        $0.image = image
+        $0.clipsToBounds = true
+        $0.contentMode = .center
+        $0.layer.cornerRadius = 16
+        $0.backgroundColor = .gray
+        return $0
+    }(UIImageView())
+
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCell()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Method
+    
+    private func setupCell() {
+        self.addSubview(buttomImage)
+      
+        buttomImage.anchor(
+            top: topAnchor,
+            left: leftAnchor,
+            bottom: bottomAnchor,
+            right: rightAnchor
+        )
+    }
+    
+}
