d58a36d67a2b7571512290882d035d5a1ad7ef80
Dinner <junghwan3588@gm.gist.ac.kr>
2022-11-11 11:26:15 +0900

    Feat:sparkles:: #109 Modify LoginViewController

--- Samsam/ViewController/LoginViewController.swift
+++ Samsam/ViewController/LoginViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    // MARK: = Property
+    
+    let loginService: LoginAPI = LoginAPI(apiService: APIService())
+    
     // MARK: - View
     
-    private let authorizationButton = ASAuthorizationAppleIDButton(authorizationButtonType: .signIn, authorizationButtonStyle: .whiteOutline)
+    private lazy var authorizationButton: ASAuthorizationAppleIDButton = {
+        $0.addTarget(self, action: #selector(login), for: .touchUpInside)
+        return $0
+    }(ASAuthorizationAppleIDButton(authorizationButtonType: .signIn, authorizationButtonStyle: .whiteOutline))
     
     // MARK: - LifeCycle
 
@@ -21,6 +28,11 @@ class LoginViewController: UIViewController {
         
         attribute()
         layout()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        requestLogin()
     }
     
     // MARK: - Method
@@ -41,5 +53,72 @@ class LoginViewController: UIViewController {
             paddingRight: 16,
             height: 50
         )
+    }
+    
+    @objc private func login() {
+        let provider = ASAuthorizationAppleIDProvider()
+        let request = provider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+    }
+    
+    private func requestLogin() {
+        Task{
+            do {
+//                let response = try await self.loginService.startAppleLogin(content: )
+                print(response as Any)
+            } catch NetworkError.serverError {
+                print("server Error")
+            } catch NetworkError.encodingError {
+                print("encoding Error")
+            } catch NetworkError.clientError(_) {
+                print("client Error")
+            }
+        }
+    }
+}
+
+extension LoginViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+
+            let userIdentifier = appleIDCredential.user
+            let userFirstName = appleIDCredential.fullName?.givenName
+            let userLastName = appleIDCredential.fullName?.familyName
+            let userEmail = appleIDCredential.email
+            
+            let appleIDProvider = ASAuthorizationAppleIDProvider()
+            appleIDProvider.getCredentialState(forUserID: userIdentifier) { (credentialState, error) in
+                switch credentialState {
+                case .authorized:
+                    DispatchQueue.main.async {
+                        self.requestPOSTWithURL(url: "http://3.39.76.231:3000/giwazip/workers", parameters: self.setLoginParameters(userIdentifier: userIdentifier, name: userLastName!+userFirstName!, email: userEmail!))
+                        
+                        // TODO: - get -> post : token 받아오기!!!
+                        self.navigationController?.pushViewController(PhoneNumViewController(), animated: true)
+                    }
+                    break
+                case .revoked:
+                    break
+                case .notFound:
+                    break
+                default:
+                    break
+                }
+            }
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print(error)
+    }
+}
+
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
     }
 }
