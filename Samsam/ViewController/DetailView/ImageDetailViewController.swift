4a55c31e4562970731381eb75f6a665bba909ea8
Juny0110 <wnsdyds403@naver.com>
2022-11-17 11:35:43 +0900

    Chore:wrench:: #119 Delete remark

--- Samsam/ViewController/DetailView/ImageDetailViewController.swift
+++ Samsam/ViewController/DetailView/ImageDetailViewController.swift
@@ -78,12 +78,4 @@ extension ImageDetailViewController: UIScrollViewDelegate {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return detailImage
     }
-//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        if scrollView.zoomScale <= 1.0 {
-//            scrollView.zoomScale = 1.0
-//        }
-//        if scrollView.zoomScale >= 3.0 {
-//            scrollView.zoomScale = 3.0
-//        }
-//    }
 }
