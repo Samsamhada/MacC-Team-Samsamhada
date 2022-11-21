1f616c32e5cae3f64825c24b6d5f519bcadb896c
taek0622 <min4644@gmail.com>
2022-11-21 16:46:36 +0900

    Feat:sparkles:: #116 Add Room Model
    - 방 정보를 가져오기 위한 모델 생성

---
+++ Network/Models/Room.swift
@@ -0,0 +1,19 @@
+//
+//  Room.swift
+//  Samsam
+//
+//  Created by 김민택 on 2022/11/21.
+//
+
+import Foundation
+
+struct Room: Decodable, Hashable {
+    let roomID: Int
+    let workerID: Int
+    let clientName: String
+    let clientNumber: String?
+    let startDate: String
+    let endDate: String
+    let warrantyTime: Int
+    let inviteCode: String
+}
