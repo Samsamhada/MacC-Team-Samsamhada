5181d191c2e48d14281d6595b43d3c5f4e10d962
Juny0110 <wnsdyds403@naver.com>
2022-11-22 10:51:06 +0900

    Resource:womans_clothes:: #123 Add PostModel

---
+++ Network/Models/Post.swift
@@ -0,0 +1,13 @@
+//
+//  Post.swift
+//  Samsam
+//
+//  Created by 지준용 on 2022/11/22.
+//
+
+import Foundation
+
+struct Post: Codable {
+    let postID, roomID, category, type: Int
+    let welcomeDescription, createDate: String
+}
