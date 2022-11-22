324e189c6a9a9e6d2f22e2d1158b44de2907a102
Juny0110 <wnsdyds403@naver.com>
2022-11-22 15:08:12 +0900

    Resource:womans_clothes:: #123 Add Photo Model

---
+++ Network/Models/Photo.swift
@@ -0,0 +1,13 @@
+//
+//  Photo.swift
+//  Samsam
+//
+//  Created by 지준용 on 2022/11/22.
+//
+
+import Foundation
+
+struct Photo: Codable {
+    let photoID, postID: Int
+    let photoPath: String
+}
