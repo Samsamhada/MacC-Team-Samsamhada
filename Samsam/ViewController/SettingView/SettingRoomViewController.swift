1ec226985e247d2844397f6bd2c5bb9f826f330b
Dinner <junghwan3588@gm.gist.ac.kr>
2022-11-23 11:00:58 +0900

    Design:iphone:: #128 Add SettingRoomViewController

---
+++ Samsam/ViewController/SettingView/SettingRoomViewController.swift
@@ -0,0 +1,94 @@
+//
+//  SettingRoomViewController.swift
+//  Samsam
+//
+//  Created by creohwan on 2022/11/23.
+//
+
+import UIKit
+
+class SettingRoomViewController: UIViewController {
+    
+    // MARK: - Property
+ 
+    private var invitecode: String?
+    
+    // MARK: - View
+    
+    private let tableView = UITableView(frame: .zero, style: .insetGrouped)
+    
+    private let header = ["",""]
+    private let data = [["초대 코드 공유"],["방 정보 수정"]]
+
+    // MARK: - LifeCycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        attribute()
+        layout()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        invitecode = "asdf" // 추후 수정 예정
+    }
+
+    // MARK: - Method
+
+    private func attribute() {
+        view.backgroundColor = .white
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+
+    private func layout() {
+        view.addSubview(tableView)
+        
+        tableView.anchor(
+            top: view.safeAreaLayoutGuide.topAnchor,
+            left: view.safeAreaLayoutGuide.leftAnchor,
+            bottom: view.safeAreaLayoutGuide.bottomAnchor,
+            right: view.safeAreaLayoutGuide.rightAnchor
+        )
+    }
+    
+    private func shareInvitecode() {
+        var shareItems = [String]()
+        if let invitecode = invitecode {
+            shareItems.append("초대 코드: \(invitecode)")
+        }
+        
+        let activityViewController = UIActivityViewController(activityItems: shareItems, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
+    }
+}
+
+extension SettingRoomViewController: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return header.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: .none)
+        cell.textLabel?.text = data[indexPath.section][indexPath.row]
+        cell.selectionStyle = .none
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath
+        {
+        case [0,0]: // 초대 코드 공유
+            self.shareInvitecode()
+        case [1,0]: // 방 정보 수정
+            print("방 정보 수정") // 추후 수정 예정
+        default:
+            break
+        }
+    }
+}
