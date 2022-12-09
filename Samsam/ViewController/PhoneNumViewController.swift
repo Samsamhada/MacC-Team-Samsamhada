3e54beadfddf960d4e8f1b0653060efc0fc7e29b
taek0622 <min4644@gmail.com>
2022-12-09 23:15:51 +0900

    Feat:sparkles:: #177 Add requestPutPhoneNumber
    - 전화번호 입력 뷰에서 전화번호 입력시 서버의 업자 전화번호 수정하는 로직 추가

--- Samsam/ViewController/PhoneNumViewController.swift
+++ Samsam/ViewController/PhoneNumViewController.swift
@@ -160,6 +160,7 @@ class PhoneNumViewController: UIViewController {
 
     @objc private func tapSubmitButton() {
         let number = "+82010" + phoneNum
+        requestPutPhoneNumber(workerID: UserDefaults.standard.integer(forKey: "workerID"), LoginDTO: LoginDTO(number: number))
         let roomListViewController = RoomListViewController()
         navigationController?.pushViewController(roomListViewController, animated: true)
     }
