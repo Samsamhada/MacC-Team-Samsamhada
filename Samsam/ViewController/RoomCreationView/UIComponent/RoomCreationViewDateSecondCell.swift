a19347a272c7f25806c2bf1a324c9bf72d56da29
Dinner <junghwan3588@gm.gist.ac.kr>
2022-12-10 15:29:49 +0900

    Fix:beetle:: #187 Change Background

--- Samsam/ViewController/RoomCreationView/UIComponent/RoomCreationViewDateSecondCell.swift
+++ Samsam/ViewController/RoomCreationView/UIComponent/RoomCreationViewDateSecondCell.swift
@@ -34,6 +34,7 @@ class RoomCreationViewDateSecondCell: UITableViewCell {
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        backgroundColor = .clear
         layout()
     }
 
